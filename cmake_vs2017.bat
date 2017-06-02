93353912a3fcdde2e4755629f69d6989fbae6ac0
ousttrue <ousttrue@gmail.com>
2017-06-02 14:42:58 +0900

    get cmake path from registry

--- cmake_vs2017.bat
+++ cmake_vs2017.bat
@@ -1,4 +1,8 @@
-set CMAKE="D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
+@echo off
+FOR /F "TOKENS=1,2,*" %%A IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7" /v "15.0"') DO IF "%%A"=="15.0" SET VSPATH=%%C
+@echo on
+
+set CMAKE="%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
 mkdir build_vs2017
 pushd build_vs2017
 %CMAKE% .. -G "Visual Studio 15 2017"
