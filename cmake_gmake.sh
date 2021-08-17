43071ef4e97ee0f4ac1c888f7c0b7916ec24576e
ousttrue <ousttrue@gmail.com>
2021-08-17 14:24:55 +0900

    remove script

--- cmake_gmake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -x
-mkdir -p build_gmake
-cd build_gmake
-cmake .. -G "Unix Makefiles"
-
--- cmake_vs2017.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-FOR /F "TOKENS=1,2,*" %%A IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7" /v "15.0"') DO IF "%%A"=="15.0" SET VSPATH=%%C
-@echo on
-
-set CMAKE="%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
-mkdir build_vs2017
-pushd build_vs2017
-%CMAKE% .. -G "Visual Studio 15 2017"
-popd
