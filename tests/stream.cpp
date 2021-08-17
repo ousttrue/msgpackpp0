46a7e8a67bc9c35da1f816aa6f94b39ed5ea542b
ousttrue <ousttrue@gmail.com>
2021-08-17 15:24:15 +0900

    stream

---
+++ tests/stream.cpp
@@ -0,0 +1,30 @@
+#include <catch.hpp>
+#include <msgpackpp.h>
+#include <stdexcept>
+
+TEST_CASE("stream")
+{
+    // packing
+    auto p = msgpackpp::packer().pack_nil().pack_nil().pack_nil().get_payload();
+
+    // push incomplete message
+    p.push_back(msgpackpp::EXT32);
+
+    // unpacking
+    auto parsed = msgpackpp::parser(p);
+    for (int i = 0; i < 3; ++i)
+    {
+        REQUIRE(parsed.is_nil());
+        parsed = parsed.next();
+    }
+
+    try
+    {
+        auto last = parsed.next();
+    }
+    catch (const std::runtime_error &e)
+    {
+        // throw if incomplete message
+        auto a = 0;
+    }
+}
